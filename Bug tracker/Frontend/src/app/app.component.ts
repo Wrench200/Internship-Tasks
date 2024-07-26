ff538aa857f2357e536eaa25b6b9888dccf84958
Tebi Njeik <tnjeik@gmail.com>
2024-07-26 22:02:32 +0100

    created some ui components in the frontend

--- Bug tracker/Frontend/src/app/app.component.ts
+++ Bug tracker/Frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, createComponent } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReportComponent } from './report/report.component';
-import { ListComponent } from './list/list.component';
+import { ReportComponent } from './components/report/report.component';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReportComponent, ListComponent],
+  imports: [RouterOutlet, ReportComponent, ListComponent, CreateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
